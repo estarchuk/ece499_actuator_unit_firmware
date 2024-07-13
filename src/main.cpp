8ece695afcf4ccec927b8793d4e2dca1f673b994
estarchuk <estarchuk@uvic.ca>
2024-07-13 15:25:04 -0700

    testing uart comm

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include "isobus/isobus/can_network_manager.hpp"
 #include "isobus/isobus/can_partnered_control_function.hpp"
 #include <iostream>
+#include <hal/uart_types.h>
+#include <driver/uart.h>
 
 #define nozzle_0 GPIO_NUM_4
 #define nozzle_1 GPIO_NUM_5
@@ -105,54 +107,43 @@ void actuator_thread(void){
     }
 }
 
+
+
 // Would like to move setup to function but too much local stuff at this time (10 July 2024)
 extern "C" void app_main()
 {
     gpio_setup();
-    /*
-    twai_general_config_t twaiConfig = TWAI_GENERAL_CONFIG_DEFAULT(isobus_tx, isobus_rx, TWAI_MODE_NORMAL);
-    twai_timing_config_t twaiTiming = TWAI_TIMING_CONFIG_250KBITS();
-    twai_filter_config_t twaiFilter = TWAI_FILTER_CONFIG_ACCEPT_ALL();
-    std::shared_ptr<isobus::CANHardwarePlugin> canDriver = std::make_shared<isobus::TWAIPlugin>(&twaiConfig, &twaiTiming, &twaiFilter);
-    std::shared_ptr<isobus::InternalControlFunction> myECU = nullptr; // A pointer to hold our InternalControlFunction
+    
+    const uart_port_t uart_num = UART_NUM_1;
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
+        .rx_flow_ctrl_thresh = 122,
+    };
 
-    isobus::CANHardwareInterface::set_number_of_can_channels(1);
-    isobus::CANHardwareInterface::assign_can_channel_frame_handler(0, canDriver);
-    isobus::CANHardwareInterface::set_periodic_update_interval(10); // Default is 4ms, but we need to adjust this for default ESP32 tick rate of 100Hz
+    // Configure UART parameters
+    ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
 
-    if (!isobus::CANHardwareInterface::start() || !canDriver->get_is_valid())
-    {
-        ESP_LOGE("AgIsoStack", "Failed to start hardware interface, the CAN driver might be invalid");
-    }
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_1, isobus_tx, isobus_rx, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
 
-    isobus::NAME TestDeviceNAME(0);
+    // Setup UART buffered IO with event queue
+    const int uart_buffer_size = (1024 * 2);
+    QueueHandle_t uart_queue;
+    // Install UART driver using an event queue here
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_1, uart_buffer_size, \
+                                            uart_buffer_size, 10, &uart_queue, 0));
 
-    //! Consider customizing some of these fields, like the function code, to be representative of your device
-    TestDeviceNAME.set_arbitrary_address_capable(true);
-    TestDeviceNAME.set_industry_group(1);
-    TestDeviceNAME.set_device_class(0);
-    TestDeviceNAME.set_function_code(static_cast<std::uint8_t>(isobus::NAME::Function::RateControl));
-    TestDeviceNAME.set_identity_number(2);
-    TestDeviceNAME.set_ecu_instance(0);
-    TestDeviceNAME.set_function_instance(0);
-    TestDeviceNAME.set_device_class_instance(0);
-    TestDeviceNAME.set_manufacturer_code(1407);
-    auto TestInternalECU = isobus::CANNetworkManager::CANNetwork.create_internal_control_function(TestDeviceNAME, 0);
-
-    isobus::CANNetworkManager::CANNetwork.add_global_parameter_group_number_callback(0xEF00, propa_callback, nullptr);
-
-    std::array<std::uint8_t, isobus::CAN_DATA_LENGTH> messageData = {1}; // Data is just all zeros
-
-    isobus::CANNetworkManager::CANNetwork.send_can_message(0xEF00, messageData.data(), isobus::CAN_DATA_LENGTH, myECU);
-    */
-
-    // Start threads after ISObus setup
-    //std::thread listener(listener_thread);
-    //std::thread actuator(actuator_thread);
+    uint8_t data[128];
+    int length = 0;
 
     while (true)
     {
         // CAN stack runs in other threads. Do nothing forever.
+        ESP_ERROR_CHECK(uart_get_buffered_data_len(uart_num, (size_t*)&length));
+        length = uart_read_bytes(uart_num, data, length, 100);
         vTaskDelay(10/portTICK_PERIOD_MS);
     }
 
