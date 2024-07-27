3decffece4e0d8d36b51196cabd3d6ffcc156c81
estarchuk <estarchuk@uvic.ca>
2024-07-27 15:29:07 -0700

    Clean up ISObus remains

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,10 @@
 #include "freertos/FreeRTOS.h"
 #include "esp_log.h"
 #include "freertos/task.h"
-#include "isobus/hardware_integration/twai_plugin.hpp"
-#include "isobus/hardware_integration/can_hardware_interface.hpp"
-#include "isobus/isobus/can_network_manager.hpp"
-#include "isobus/isobus/can_partnered_control_function.hpp"
 #include <iostream>
 #include <hal/uart_types.h>
 #include <driver/uart.h>
+#include <driver/gpio.h>
 
 #define nozzle_0 GPIO_NUM_4
 #define nozzle_1 GPIO_NUM_5
@@ -15,14 +12,6 @@
 #define nozzle_3 GPIO_NUM_7
 #define isobus_tx GPIO_NUM_10
 #define isobus_rx GPIO_NUM_9
-
-// Global queues
-std::queue<int> receiver = {};
-std::queue<int> command_queue = {};
-std::queue<int> actuator_queue = {};
-std::mutex receiver_mutex;
-std::mutex command_mutex;
-std::mutex actuator_mutex;
 
 int nozzle_state_0 = 0;
 int nozzle_state_1 = 0;
@@ -49,65 +38,6 @@ void gpio_setup(void){
     gpio_set_level(nozzle_3, 0);
 
 }
-
-void propa_callback(const isobus::CANMessage &CANMessage, void *){
-
-    std::cout << CANMessage.get_data_length() << std::endl;
-    
-    // Both of these methods might work, needs testing
-    std::vector message_data = CANMessage.get_data();
-    receiver.push(message_data.at(0));
-
-    receiver.push(CANMessage.get_data().at(0));
-
-}
-
-void listener_thread(void){
-
-    while(1){
-        if(!receiver.empty()){
-            receiver_mutex.lock();
-            command_mutex.lock();
-            command_queue = receiver;
-            receiver = {};
-            command_mutex.unlock();
-            receiver_mutex.unlock();
-        }
-        // 10 ms delay, required otherwise the MCU locks up
-        vTaskDelay(10/portTICK_PERIOD_MS);
-    }
-}
-
-void actuator_thread(void){
-
-    while(1){
-        if(!command_queue.empty()){
-            command_mutex.lock();
-            actuator_queue = command_queue;
-            command_queue = {};
-            command_mutex.unlock();
-        }
-
-        // Replace this with a while loop?
-        if(!actuator_queue.empty()){
-            int command = actuator_queue.front();
-            actuator_queue.pop();
-            switch(command){
-                case 0:
-                    gpio_set_level(nozzle_0, !nozzle_state_0);
-                    nozzle_state_0 = !nozzle_state_0;
-                    break;
-                default:
-                    // Do nothing if invalid command received
-                    break;
-            }
-        }
-        // 10 ms delay, required otherwise the MCU locks up
-        vTaskDelay(10/portTICK_PERIOD_MS);
-    }
-}
-
-
 
 // Would like to move setup to function but too much local stuff at this time (10 July 2024)
 extern "C" void app_main()
@@ -173,10 +103,7 @@ extern "C" void app_main()
             }
         }
 
-        //length = uart_read_bytes(uart_num, data, length, 100);
-        //printf("%s\n", data);
         vTaskDelay(10/portTICK_PERIOD_MS);
     }
 
-    isobus::CANHardwareInterface::stop();
 }
